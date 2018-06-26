3bb184e3ab8880f9865da651813c83b78e0fc309
SRoller <sroller@justfab.com>
2018-06-26 09:14:47 -0400

    makes some more changes to layout

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ export default class App extends React.Component {
       toolbarButton, 
       toolbarTitle, 
       mainContainer, 
-      content 
+      content,
+      messageBox,
+      messageBoxBodyText,
+      messageBoxTitleText
     } = styles;
 
     return (
@@ -19,7 +22,18 @@ export default class App extends React.Component {
           <Text style={toolbarButton}>Like</Text>
         </View>
         <View style={content}>
-          <Text>This is content</Text>
+          <View style={messageBox}>
+            <View>
+              <Text style={messageBoxTitleText}>
+                A simple message
+              </Text>
+            </View>
+            <View>
+              <Text style={messageBoxBodyText}>
+                This is a longer message that will allow us to see the alignment in action.
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     );
@@ -49,6 +63,29 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    backgroundColor: '#ebe'
+    backgroundColor: '#ebe',
+    alignItems: 'flex-end', // flex-start, flex-end, stretch, center
+    flexDirection: 'row'
+    // , justifyContent: 'center'
+  },
+  messageBox: {
+    backgroundColor: '#ef553a',
+    width: 300,
+    paddingTop: 10,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 10
+  },
+  messageBoxTitleText: {
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10
+  },
+  messageBoxBodyText: {
+    color: '#fff',
+    fontSize: 16
   }
 });
